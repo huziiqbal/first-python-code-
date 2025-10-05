211734c6ecce143968fe5368d7c4d7e913774ebf
Huzaifa Iqbal <huziiqbal00@gmail.com>
2025-10-05 15:25:10 +0530

    solved some problems in code

--- CodeDecode.py
+++ CodeDecode.py
@@ -4,7 +4,7 @@ purpose = input(" do you want to decode or encode a message?")
 
 if purpose == "encode":
   
-  message = input("enter your message without space in between words")
+  message = input("enter your message ")
   if len(message) % 2 == 0:
     k = len(message) // 2
     p = message[0:k]
