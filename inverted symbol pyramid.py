a140c55c9461e67e6c59f341ba3c4a033b4f26ec
Huzaifa Iqbal <huziiqbal00@gmail.com>
2025-10-08 19:30:35 +0530

    added a new pattern to print inverted  pyramid

--- inverted symbol pyramid.py
+++ inverted symbol pyramid.py
@@ -1,3 +1,4 @@
+#type 1 pyramid pattern(in which number of stars in eachrow = 2*row-1)
 n = int(input ("enter the number of lines of your inverted pyramid "))
 d=[]
 s=" "
@@ -12,4 +13,19 @@ w=0
 while w<=n-2:
   
   print(s*(w+1),*d[(w+1):(m-w-1) ])
+  w=w+1
+
+#type 2 pyramid pattern(in which number of stars in each row = row)
+n = int(input ("enter the number of lines of your inverted pyramid "))
+d=[]
+s=" "
+m=n
+for y in range(0,m):
+  
+  d.append("*")
+
+w=0
+while w<=n:
+  
+  print(s*(w+1),*d[(w):(m)])
   w=w+1
