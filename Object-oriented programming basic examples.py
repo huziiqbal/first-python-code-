86e452c8705e86c841d10a9ba2634bf31130962c
Huzaifa Iqbal <huziiqbal00@gmail.com>
2025-10-15 18:44:19 +0530

    added some more questions based on OOP

--- Object-oriented programming basic examples.py
+++ Object-oriented programming basic examples.py
@@ -53,3 +53,55 @@ while True:
     t=int(input("Do you want area of another rectangle ?\n" "if yes then press 1 and if no then press 0 " ))
     if t==0:
        break
+
+    # NUMBER 3
+  
+class Person :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def info (self):
+        print(f'hello my name is {self.name} and I am {self.age} years old')
+a=Person("HUZAIFA",20)
+a.info()
+
+class Person :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def info (self):
+        print(f'hello my name is {self.name} and I am {self.age} years old')
+a=Person("HUZAIFA",20)
+a.info()
+
+       #NUMBER 4
+
+class Circle :
+     def __init__(self,radius):
+        self.radius = radius
+     def area_circumference(self):
+         print(f'area of the circle whose radius you gave is {3.14*self.radius*self.radius} Units')
+
+a=Circle(20)
+a.area_circumference()
+
+            #  NUMBER 5
+
+class Students:
+    def __init__(self,name,marks_of_english,marks_of_maths,marks_of_science):
+       self.n = name
+       self.m  =  marks_of_english
+       self.e =  marks_of_maths
+       self.s =  marks_of_science
+    def Average_Grade(self):
+        k=(self.m +self.e +self.s)//3
+        if k>=90:
+         grade = "A"
+        elif k>=75:
+         grade = "B"
+        else:
+         grade = "C"
+        print(f'The average marks of {self.n} are {k}.\nAnd the grade is : {grade}')
+   
+Student_one=Students("HUZAIFA",87,93,98)
+Student_one.Average_Grade()
