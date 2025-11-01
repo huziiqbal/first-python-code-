ce30e2611a5d62f7fb78055ac2e4a98e2258bc1f
Your Name <you@example.com>
2025-11-02 01:48:54 +0530

    made manually converter of binary to 1's and 2's complement

---
+++ 1's comp calculator.py
@@ -0,0 +1,9 @@
+binary_number =input("Enter the binary number you want ones complement of :")
+g=[]
+for y in binary_number:
+    if y == '0':
+        g.append('1')
+    elif y == '1':
+        g.append('0')
+
+print(f'ones complement of {binary_number} is : {"".join(g)}')
---
+++ 2's comp calculator.py
@@ -0,0 +1,27 @@
+binary_number =input("Enter the binary number you want two's complement of :")
+g=[]
+for y in binary_number:
+    if y == '0':
+        g.append('1')
+    elif y == '1':
+       g.append('0')
+f= len(g)
+s=[]
+k=[]
+h = g[::-1]
+for u in h:
+    if u == '1':
+        k.append('0')
+    elif u == '0' :
+        k.append('1')
+        break
+p = f-(len(k)) -1
+for r in range(p):
+ i = 0
+for i in range (p+1):
+        s.append(g[i])
+        i= i+1
+
+v = "" . join(s)
+w ="" .join(k[::-1])
+print(f"The Two's complement of {binary_number}  is : {v+w}")
