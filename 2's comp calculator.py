dfb5504a56b4d28feaa60b573e6698c40f6a9247
Your Name <you@example.com>
2025-11-02 18:51:25 +0530

    commented the logic behing the calculator and removed some unnecessary code lines

--- 2's comp calculator.py
+++ 2's comp calculator.py
@@ -1,3 +1,6 @@
+# Logic: First take the 1's complement by flipping all bits,
+# then manually add 1 by flipping bits from the right until the first 0 is found,
+# to get the final 2's complement.
 binary_number =input("Enter the binary number you want two's complement of :")
 g=[]
 for y in binary_number:
@@ -16,12 +19,10 @@ for u in h:
         k.append('1')
         break
 p = f-(len(k)) -1
-for r in range(p):
- i = 0
+i = 0
 for i in range (p+1):
         s.append(g[i])
         i= i+1
-
 v = "" . join(s)
 w ="" .join(k[::-1])
 print(f"The Two's complement of {binary_number}  is : {v+w}")
